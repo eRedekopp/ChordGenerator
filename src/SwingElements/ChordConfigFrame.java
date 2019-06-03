d1f7a6faf805347ea31c2d2e55c900740fa423d4
eRedekopp <eric.redekopp@mail.com>
2019-06-02 23:39:36 -0600

    ChordConfigLayout form built

--- src/SwingElements/ChordConfigFrame.java
+++ src/SwingElements/ChordConfigFrame.java
@@ -1,15 +1,27 @@
 package SwingElements;
 
 import javax.swing.*;
-import java.awt.*;
 
-/**
- * A frame for setting up the chords to be displayed
- */
-public class ChordConfigFrame extends JFrame {
-
-    public ChordConfigFrame() {
-
-    }
-
+public class ChordConfigFrame {
+    private JPanel panel1;
+    private JComboBox comboBox3;
+    private JComboBox comboBox4;
+    private JTextField textField1;
+    private JComboBox comboBox5;
+    private JComboBox comboBox6;
+    private JTextField textField2;
+    private JComboBox comboBox7;
+    private JComboBox comboBox8;
+    private JTextField textField3;
+    private JComboBox comboBox9;
+    private JComboBox comboBox10;
+    private JComboBox comboBox11;
+    private JButton enterButton;
+    private JButton helpButton;
+    private JComboBox comboBox1;
+    private JButton enterButton1;
+    private JCheckBox allowDuplicateNotesCheckBox;
+    private JComboBox comboBox2;
+    private JTextField textField4;
+    private JButton findChordsButton;
 }
